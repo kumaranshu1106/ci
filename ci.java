feeeeb73bc5fdc2309ae48912b531729c02e63bb
kumaranshu1106 <114850574+kumaranshu1106@users.noreply.github.com>
2022-10-11 18:31:34 +0530

    Create ci.java

---
+++ ci.java
@@ -0,0 +1,18 @@
+import java.util .*;
+class sici
+{
+    public static void main (String argu[ ])
+{
+    double pr, rate, t, sim,com;
+    Scanner sc=new Scanner (System. in);
+    System.out.println("Enter the amount:");
+    pr=sc.nextDouble();
+    System. out. println("Enter the No.of years:");
+    t=sc.nextDouble();
+    System. out. println("Enter the Rate of  interest");
+    rate=sc.nextDouble();
+    //please help
+    System.out.println("Simple Interest="+sim);
+    System.out. println("Compound Interest="+com);
+   }
+}
